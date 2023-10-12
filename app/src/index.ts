a8f463f88a5d389f7d90d756fb857a2c9a3e8093
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-10-12 14:58:02 +0200

    clean up

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,47 +1,25 @@
-
 const url = "http://localhost:8081/";
-document.addEventListener("DOMContentLoaded", initApp);
+
+let appContent: Element;
+
 function initApp() {
   const navHome = document.getElementById("nav-home");
   if (!navHome) {
     return;
   }
-  const appContent = document.getElementsByTagName("app-content")[0];
+
+  navHome.addEventListener("click", () => {
+    request(url + "button", appContent);
+  });
+
+  appContent = document.getElementsByTagName("app-content")[0];
   if (!appContent) {
     return;
   }
 
-  
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type === 'childList') {
-        console.log('Content changed!');
-        // Get all elements that have data-function attribute
-        const elements = document.querySelectorAll('[data-function]');
-        console.log(elements);
-        // attach a click event listener to each element
-        elements.forEach((element) => {
-          const functionName = element.getAttribute('data-function');
-          console.log(functionName);
-          const isListenerAttached = element.getAttribute('data-listener-attached');
-          if (isListenerAttached) {
-            return;
-          }
-          element.setAttribute('data-listener-attached', 'true');
-          element.addEventListener('click', () => {
-            request(url + functionName, appContent);
-          });
+  const observer = new MutationObserver(observeContent);
 
-        });
-      }
-    });
-  });
-  
   observer.observe(appContent, { childList: true, subtree: true });
-  
-  navHome.addEventListener("click", () => {
-    request(url + "button", appContent);
-  });
 }
 
 function request(url: string, rootElement: Element) {
@@ -52,3 +30,45 @@ function request(url: string, rootElement: Element) {
       rootElement.innerHTML = html;
     });
 }
+function observeContent(mutations: any[]) {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      // Get all elements that have data-function attribute
+      const elements = document.querySelectorAll("[data-function]");
+
+      // attach a click event listener to each element
+      elements.forEach((element) => {
+        const functionName = element.getAttribute("data-function");
+
+        let eventName: string;
+        if (null === element.getAttribute("data-event")) {
+          eventName = "click";
+        } else {
+          eventName = element.getAttribute("data-event")!;
+        }
+
+        let targetID: string;
+        if (null === element.getAttribute("data-target")) {
+          targetID = "";
+        } else {
+          targetID = element.getAttribute("data-target")!;
+        }
+
+        console.log(functionName);
+        const isListenerAttached = element.getAttribute(
+          "data-listener-attached"
+        );
+
+        if (isListenerAttached) {
+          return;
+        }
+        element.setAttribute("data-listener-attached", "true");
+        element.addEventListener(eventName, () => {
+          request(url + functionName, appContent);
+        });
+      });
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
