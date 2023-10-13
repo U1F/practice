00f93c635fe0889a1a91a82d84f9c540b18ac87f
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-10-13 11:29:23 +0200

    testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module grapefruixyz.org/m/v2
 
 go 1.21.1
+
+require github.com/a-h/templ v0.2.364
--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-
 	"log"
 	"net/http"
 	"os"
+	"github.com/a-h/templ"
 )
 
 func main() {
-	endpoints := map[string]string{
-		"/home":    "home.html",
-		"/about":   "about.html",
-		"/contact": "contact.html",
-	}
+	// for the main path we want to serve index.html
+	http.HandleFunc("/", responseFromFile("index.html"))
+// Serve static files from a "static" directory
+	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("dist"))))
 
-	for endpoint, filePath := range endpoints {
-		http.HandleFunc(endpoint, generateHandler(filePath))
-	}
+	component := hello("Ulf")
+	http.Handle("/home", templ.Handler(component))
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
 
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
@@ -25,7 +25,23 @@ func main() {
 	}
 }
 
-func generateHandler(filePath string) http.HandlerFunc {
+
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	respond(w, r, "<h1>About Us</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	respond(w, r, "<h1>Contact</h1>")
+}
+
+func respond(w http.ResponseWriter, r *http.Request, html string) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprint(w, html)
+}
+
+func responseFromFile(filePath string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//Read from file
 		html, err := os.ReadFile(filePath)
@@ -35,10 +51,4 @@ func generateHandler(filePath string) http.HandlerFunc {
 		}
 		respond(w, r, string(html))
 	}
-}
-
-func respond(w http.ResponseWriter, r *http.Request, html string) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprint(w, html)
 }
